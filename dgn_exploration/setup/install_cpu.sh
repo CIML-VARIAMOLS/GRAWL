e5ddec751fd83582d44678183cc810bb5b472c2f
marco giulini <marcogiulini@MacBook-Air-di-marco-2.local>
2021-02-02 17:08:01 +0100

    changed setup files

--- dgn_exploration/setup/install_cpu.sh
+++ dgn_exploration/setup/install_cpu.sh
@@ -2,12 +2,19 @@
 #conda create -n GRAWL python=3.7
 #conda activate GRAWL
 
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    pip install torch==1.4.0+cpu -f https://download.pytorch.org/whl/torch_stable.html
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-    # Mac OSX
-    pip install torch==1.4.0
+conda_env=$( conda env list | grep "\*" | grep GRAWL | wc -l | bc )
+echo "conda_env = ${conda_env}"
+if [[ ${conda_env} == 0 ]]; then
+	echo "GRAWL environment not successfully created or activated, aborting.."
+	exit
+elif [[ ${conda_env} == 1 ]]; then
+	echo "GRAWL environment activated, installing packages..."
+	if [[ "$OSTYPE" == "linux-gnu" ]]; then
+	    pip install torch==1.4.0+cpu -f https://download.pytorch.org/whl/torch_stable.html
+	elif [[ "$OSTYPE" == "darwin"* ]]; then
+    	# Mac OSX
+    	    pip install torch==1.4.0
+	fi
+	pip install -r requirements.txt
+	pip install -r other_requirements_cpu.txt --find-links https://pytorch-geometric.com/whl/torch-1.4.0.html
 fi
-
-pip install -r requirements.txt
-pip install -r other_requirements_cpu.txt --find-links https://pytorch-geometric.com/whl/torch-1.4.0.html
--- dgn_exploration/setup/install_gpu.sh
+++ dgn_exploration/setup/install_gpu.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-conda create -n GRAWL_gpu python=3.7
-conda activate GRAWL_gpu
+#conda create -n GRAWL_gpu python=3.7
+#conda activate GRAWL_gpu
 
-conda install -c pytorch=1.4.0+cu100 torchvision cudatoolkit=10.0 pytorch -f https://download.pytorch.org/whl/torch_stable.html
+conda_env=$( conda env list | grep "\*" | grep GRAWL_gpu | wc -l | bc )
+echo "conda_env = ${conda_env}"
 
-pip install -r requirements.txt
-pip install -r other_requirements_gpu.txt --find-links https://pytorch-geometric.com/whl/torch-1.4.0.html
+if [[ ${conda_env} == 0 ]]; then
+        echo "GRAWL environment not successfully created or activated, aborting.."
+        exit
+elif [[ ${conda_env} == 1 ]]; then
+        echo "GRAWL environment activated, installing packages..."
+	conda install -c pytorch=1.4.0+cu100 torchvision cudatoolkit=10.0 pytorch -f https://download.pytorch.org/whl/torch_stable.html
+
+	pip install -r requirements.txt
+	pip install -r other_requirements_gpu.txt --find-links https://pytorch-geometric.com/whl/torch-1.4.0.html
+fi
